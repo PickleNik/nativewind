6bbb3f47436091c7c96f7252fdbfe16518cced5e
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-02-26 18:31:37 -0500

    init new docs

---
+++ apps/new-website/app/layout.config.tsx
@@ -0,0 +1,27 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { Logo } from './assets';
+
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: (
+      <>
+        <Logo />
+        NativeWind
+      </>
+    ),
+  },
+  links: [
+    {
+      text: 'Documentation',
+      url: '/docs',
+      active: 'nested-url',
+    },
+  ],
+};
