6d38906d70eacbbb67aff52d9dd07a0220a0e4a0
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-03-26 01:51:37 -0400

    separate docs & home layout configs

--- apps/new-website/app/(home)/layout.tsx
+++ apps/new-website/app/(home)/layout.tsx
@@ -1,7 +1,115 @@
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+import { Logo } from '../assets';
+import { BookText, Book, Cpu, Heart, Layout as LayoutIcon, LayoutTemplate, Server, UsersRound } from 'lucide-react';
+import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return <HomeLayout
+    githubUrl='https://github.com/nativewind/nativewind'
+    nav={{
+      transparentMode: 'top',
+      title: (
+        <>
+          <Logo />
+          NativeWind
+          <RootToggle
+            // onClick={(e) => {
+            //   e.preventDefault();
+            //   e.stopPropagation();
+            // }}
+            className="rounded-full border pr-2 cursor-pointer"
+            options={[
+              
+              {
+                title: 'v4',
+                url: '/docs',
+              },
+              {
+                title: 'v2',
+                url: 'https://www.nativewind.dev/v2/',
+              },
+            ]}
+          />
+        </>
+      ),
+    }}
+    links={[
+      // NOTE: docs link is separate to only show in home layout and not in docs layout, linkItems are passed to docs/layout.tsx
+      {
+        // NOTE: for /layouts/notebook option https://fumadocs.vercel.app/docs/ui/layouts/docs#nav-mode
+        // on: 'nav',
+        type: 'menu',
+        text: 'Docs',
+        url: '/docs',
+        // active: 'nested-url',
+        // NOTE: homepage hover items
+        items: [
+          {
+            menu: {
+              banner: (
+                <div className="-mx-3 -mt-3">
+                  <div className="bg-cyan-500/20 rounded-t-xl w-full h-64"></div>
+                </div>
+              ),
+              className: 'md:row-span-2',
+            },
+            text: 'Overview',
+            description: 'Overview',
+            url: '/docs/',
+          },
+          {
+            icon: <Book />,
+            text: 'Guides',
+            description: 'Troubleshooting',
+            url: '/docs/guides/troubleshooting',
+          },
+          {
+            icon: <Cpu />,
+            text: 'Core Concepts',
+            description: 'Built on TailwindCSS',
+            url: '/docs/core-concepts/tailwindcss',
+          },
+          {
+            icon: <Server />,
+            text: 'API',
+            description: 'Nativewind ApI',
+            url: '/docs/layouts',
+          },
+          {
+            icon: <LayoutIcon />,
+            text: 'Customization',
+            description: 'Configuration',
+            url: '/docs/customization/configuration',
+          },
+        ],
+      },
+      {
+        icon: <BookText />,
+        text: 'Blog',
+        url: '/blog',
+        active: 'nested-url',
+      },
+      {
+        text: 'Community',
+        // TODO: GH Discussions / Discord Link?
+        url: '/community',
+        icon: <UsersRound />,
+        external: true,
+      },
+      {
+        text: 'Showcase',
+        url: '/showcase',
+        icon: <LayoutTemplate />,
+        active: 'url',
+      },
+      {
+        text: 'Sponsors',
+        url: '/sponsors',
+        icon: <Heart />,
+      },
+    ]}
+  >
+    { children }
+  </HomeLayout >;
 }
--- apps/new-website/app/docs/layout.tsx
+++ apps/new-website/app/docs/layout.tsx
@@ -1,72 +1,121 @@
-import { DocsLayout, DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import type { ReactNode } from 'react';
-import { baseOptions, linkItems } from '@/app/layout.config';
 import { source } from '@/lib/source';
+import { Logo } from '../assets';
 import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle';
+import Link from 'next/link';
+import { BookOpen, BookText, Heart, LayoutTemplate, UserRound } from 'lucide-react';
 
-const docsOptions: DocsLayoutProps = {
-  ...baseOptions,
-  tree: source.pageTree,
-  links: linkItems,
-  // NOTE for /layouts/notebook option https://fumadocs.vercel.app/docs/ui/layouts/docs#notebook
-  // nav: {
-  //   ...baseOptions.nav,
-  //   transparentMode: 'top',
-  //   mode: 'top',
-  // },
-  // tabMode: 'navbar',
-  sidebar: {
-    collapsible: false,
-    // NOTE: for 2+ root:true metas https://fumadocs.vercel.app/docs/ui/layouts/docs#sidebar-tabs
-    tabs: {
-      transform(option, node) {
-        const meta = source.getNodeMeta(node);
-        if (!meta) return option;
-
-        const color = `var(--${meta.file.dirname}-color, var(--color-fd-foreground))`;
-
-        return {
-          ...option,
-          icon: (
-            <div
-              className="rounded-md p-1 shadow-lg ring-2 [&_svg]:size-5"
-              style={
-                {
-                  color,
-                  border: `1px solid color-mix(in oklab, ${color} 50%, transparent)`,
-                  '--tw-ring-color': `color-mix(in oklab, ${color} 20%, transparent)`,
-                } as object
-              }
-            >
-              {node.icon}
-            </div>
-          ),
-        };
-      },
-    },
-    banner: (
-      <RootToggle
-        options={[
-          
-          {
-            title: 'v4',
-            description: 'Latest Version',
-            url: '/docs',
-          },
-          {
-            title: 'v2',
-            description: 'v2 Docs',
-            url: 'https://www.nativewind.dev/v2/',
-          },
-        ]}
-      />
-    ),
-  },
-};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout {...docsOptions}>
+    <DocsLayout
+      // themeSwitch={{
+      //   mode: 'light-dark',
+      // }}
+      tree={source.pageTree}
+      // links: [],
+      githubUrl='https://github.com/nativewind/nativewind'
+      // NOTE: for /layouts/notebook option https://fumadocs.vercel.app/docs/ui/layouts/docs#notebook
+      nav={{
+        transparentMode: 'top',
+        title: (
+          <>
+            <Logo />
+            NativeWind
+            <RootToggle
+              // onClick={(e) => {
+              //   e.preventDefault();
+              //   e.stopPropagation();
+              // }}
+              className="rounded-full border pr-2 cursor-pointer"
+              options={[
+            
+                {
+                  title: 'v4',
+                  url: '/docs',
+                },
+                {
+                  title: 'v2',
+                  url: 'https://www.nativewind.dev/v2/',
+                },
+              ]}
+            />
+          </>
+        ),
+        mode: 'top',
+      }}
+      // TODO: remove, probably won't be used
+      // tabMode: 'navbar',
+      sidebar={{
+        collapsible: false,
+        // NOTE: for 2+ root:true metas https://fumadocs.vercel.app/docs/ui/layouts/docs#sidebar-tabs
+        // TODO: remove, probably won't be used
+        tabs: {
+          transform(option, node) {
+            const meta = source.getNodeMeta(node);
+            if (!meta) return option;
+
+            const color = `var(--${meta.file.dirname}-color, var(--color-fd-foreground))`;
+
+            return {
+              ...option,
+              icon: (
+                <div
+                  className="rounded-md p-1 shadow-lg ring-2 [&_svg]:size-5"
+                  style={
+                    {
+                      color,
+                      border: `1px solid color-mix(in oklab, ${color} 50%, transparent)`,
+                      '--tw-ring-color': `color-mix(in oklab, ${color} 20%, transparent)`,
+                    } as object
+                  }
+                >
+                  {node.icon}
+                </div>
+              ),
+            };
+          },
+        },
+        // banner: (
+        //   <div className="flex flex-col">
+        //     <Link href="/docs" className="relative flex flex-row items-center gap-2 rounded-md p-2 text-start [overflow-wrap:anywhere] md:py-1.5 [&_svg]:size-4 [&_svg]:shrink-0 bg-fd-primary/10 text-fd-primary" style={{ paddingInlineStart: 'calc(var(--spacing) * 2)' }} >
+        //       <BookOpen />
+        //       Docs
+        //     </Link>
+        //     <Link href="/blog" className="relative flex flex-row items-center gap-2 rounded-md p-2 text-start text-fd-muted-foreground [overflow-wrap:anywhere] md:py-1.5 [&_svg]:size-4 [&_svg]:shrink-0 transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none" style={{ paddingInlineStart: 'calc(var(--spacing) * 2)' }} >
+        //       <BookText />
+        //       Blog
+        //     </Link>
+        //     <Link href="/community" className="relative flex flex-row items-center gap-2 rounded-md p-2 text-start text-fd-muted-foreground [overflow-wrap:anywhere] md:py-1.5 [&_svg]:size-4 [&_svg]:shrink-0 transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none" style={{ paddingInlineStart: 'calc(var(--spacing) * 2)' }} >
+        //       <UserRound />
+        //       Community
+        //     </Link>
+        //     <Link href="/showcase" className="relative flex flex-row items-center gap-2 rounded-md p-2 text-start text-fd-muted-foreground [overflow-wrap:anywhere] md:py-1.5 [&_svg]:size-4 [&_svg]:shrink-0 transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none" style={{ paddingInlineStart: 'calc(var(--spacing) * 2)' }} >
+        //       <LayoutTemplate />
+        //       Showcase
+        //     </Link>
+        //     <Link href="/sporsor" className="relative flex flex-row items-center gap-2 rounded-md p-2 text-start text-fd-muted-foreground [overflow-wrap:anywhere] md:py-1.5 [&_svg]:size-4 [&_svg]:shrink-0 transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none" style={{ paddingInlineStart: 'calc(var(--spacing) * 2)' }} >
+        //       <Heart />
+        //       Sponsor
+        //     </Link>
+        //     {/* <RootToggle
+        //       options={[
+            
+        //         {
+        //           title: 'v4',
+        //           url: '/docs',
+        //         },
+        //         {
+        //           title: 'v2',
+        //           url: 'https://www.nativewind.dev/v2/',
+        //         },
+        //       ]}
+        //     /> */}
+        //   </div>
+        // ),
+      }}
+    >
       {children}
     </DocsLayout>
   );
